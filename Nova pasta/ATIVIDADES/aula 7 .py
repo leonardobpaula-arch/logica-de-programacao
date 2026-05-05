e4a9603dc39bd0dc5d5d7cb66ff6330a66a7c50f
Leonardo Bertanha <leonardo.b.paula@edu.senai.br>
2026-05-05 11:23:19 -0300

    atualizações da aula

--- Nova pasta/ATIVIDADES/aula 7 .py
+++ Nova pasta/ATIVIDADES/aula 7 .py
@@ -1,1 +1,71 @@
 # Projeto cancela automática
+
+#variaveis
+
+# Projeto 1:
+# Projeto: Precisamos de um algoritmo para gerenciamento de cancelas para um shopping.
+# Toda entrada e saída irá ser sinalizada
+# Valores para entrada e permanência do veículo deverá ser pergutado
+# As entrada deverão ser registradas por placa.
+#
+# Passo 1:  
+# Perguntar informações sobre o veiculo ou forma acesso
+# Pressionar o botao para emitir ticket
+# Verificar se possui TAG para acesso liberado
+# Se possuir erros informar ao usuário
+
+# Passo 2:
+# Verificar tempo de permanência
+# Valor a ser cobrado
+
+# Passo 3:
+# Saída como será?
+# Calcular tempo de permanência
+# Se for TAG gerar na fatura da TAG
+# Pagar ticket
+# Devolver ticket na saída
+
+# Passo 4:
+# Gerar relatório de entradas e saídas
+# Tratamento de Erros
+# Revisão do código
+
+print("Bem vindo")
+print("Qual é o modelo do veiculo e a placa do carro?")
+
+modelo = input("Insira o modelo do veículo")
+
+placa = input("Insira a placa do carro")
+
+print("Como deseja entrar?")
+
+entrada = input("Opção 1 Ticket/Opção 2 TAG/Opção 3 interfone")
+
+if entrada == "ticket":
+    print("Acesso liberado")
+    
+    hora_entrada = float(input("Digite a hora de entrada"))
+    estacionamento = float(input("Qual o custo do estacionamento?"))
+    hora_saida = float(input("Quando você deixou o estacionamento"))
+
+    permanencia = hora_saida - hora_entrada
+
+    print(f"Seu tempo de permanência foi de {permanencia}")
+
+    total_pagar = permanencia * estacionamento
+
+    print(f"O valor da estadia será R${total_pagar:.2f}")
+    print("Por favor devolva o ticket")
+
+
+elif entrada == "TAG":
+    print("Acesso liberado")
+    print("A cobrança será realizada diretamente à sua fatura")
+
+
+elif entrada == "interfone":
+    print("Liberando acesso pelo interfone")
+    print("Ao sair, use o interfone novamente")
+
+else:
+    print("Obrigado pela visita. Volte sempre")
---
+++ Nova pasta/ATIVIDADES/aula 8 .py
@@ -0,0 +1,238 @@
+# Exercícios de Programação Python: "O Caça-Erros"
+
+# 1. O Problema da Idade
+
+# idade = input("Digite sua idade:")
+
+# if idade >= 18:
+#     print("Você é maior de idade.")
+
+
+# correção
+
+
+# idade = int(input("Digite sua idade"))
+
+# if idade >= 18:
+#     print("Você é maior de idade")
+
+
+# melhorado
+
+
+# idade = int(input("Digite sua idade"))
+
+# if idade >= 18:
+#     print("Você é maior de idade")
+
+# else:
+#     print("Você é menor de idade")
+
+
+# 2. A Escrita Fiel
+
+# nome = "Mariana"
+# print("Seja bem vinda, nome!")
+
+# correção
+
+# nome = "Mariana"
+
+# print("Seja bem vinda", nome, "!")
+
+# 3 melharado
+
+# nome = input("Digite seu nome")
+
+# print("Seja bem vinda", nome, "!")
+
+
+# 3. Falta de Espaço
+
+# numero = 10
+
+# if numero > 5:
+#     print("O número é maior que cinco.")
+
+# else:
+#     print("O número é menor que ou igual a cinco.")
+
+# correção 
+
+# numero = int(input("Digite um numero entre 0 a 10"))
+
+# if numero >= 6:
+#     print("O número é maior que cinco.")
+
+# else:
+#     print("O número é menor ou igual a cinco.")
+
+
+
+
+
+
+
+
+
+
+
+
+# 4. Esquecimento Fatal
+
+
+# usuario = "aluno123"
+# if usuario == "aluno123"
+# print("Login realizado com sucesso")
+
+
+# correção
+
+
+# usuario = "aluno123"
+
+# if usuario == "aluno123":
+#     print("Login realizado com sucesso")
+
+
+# melhorado
+
+
+# usuario = input("Digite sua senha")
+
+# if usuario == "aluno123":
+#     print("Login realizado com sucesso")
+
+# else:
+#     print("Senha incorreta tente novamente")
+
+
+# 5. Atribuição vs. Comparação
+
+
+# clima = "ensolarado"
+# if clima = "chuvoso":
+# print("Leve um guarda-chuva!")
+
+
+# correção
+
+
+# clima = "ensolarado"
+
+# if clima == "chuvoso":
+#     print("Leve um guarda chuva!")
+
+
+# melhorado
+
+
+# clima = input("Como está o clima?")
+
+# if clima == "Chuvoso":
+#     print("Leve um guarda chuva")
+
+# elif clima == "Ensolarado":
+#     print("Leve um óculos de sol")
+
+# else:
+#     print("Aproveite o dia")
+
+
+# 6. Misturando Alhos com Bugalhos
+
+
+# pontos = 50
+# print("Parabéns! Você fez" + pontos+"pontos.")
+
+
+#correção
+
+
+# pontos = 50
+# print("Parabéns! Você fez", pontos,"pontos")
+
+
+#melhorado
+
+
+# pontos = input("Digite quantos pontos você ganhou")
+# print("Parabéns! Você fez", pontos,"pontos")
+
+
+
+# 7. A Ordem dos Fatores
+# O sistema deve dar "exelente" para notas 9 ou 10.
+
+
+# nota = 9.5
+
+# if nota >=7:
+#     print("Aprovado")
+
+# elif nota >=9:
+#     print("Excelente!")
+
+
+#correção
+
+
+# nota = 9.5
+
+# if 7 >= nota <=9:
+#     print("Aprovado")
+
+# elif nota >=9:
+#     print("Excelente!")
+
+
+# melhorado
+
+
+# nota = float(input("Digite sua nota de 0 a 10"))
+
+# if nota <= 4.5:
+#     print("Recuperção")
+
+# elif 5 >= nota <=7:
+#     print("Na média")
+
+# elif 7.5 >= nota <=9:
+#     print("Aprovado")
+
+# elif nota >=9:
+#     print("Excelente")
+
+# else:
+#     print("algo deu errado! insira outro valor")
+
+
+
+# 8. O Contador de 1 a 5
+
+# for i in range(5):
+#     print(i)
+
+#correção
+
+
+# for i in range(1,6):
+#     print(i)
+
+
+#melhorado
+
+# for i in range(1,6):
+#     print("número", (i), "...")
+
+
+# 9. O Loop Eterno
+
+tentativas = 1
+while tentativas <= 3:
+    print("Tentando conectar...")
+
+
+#correção
+
+
